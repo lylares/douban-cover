fed966fbdde14b77d21681d0ad0499c67c3fce35
lylares <contact@lylares.com>
2019-03-20 15:58:56 +0800

    Update douban-cover.php

--- douban-cover.php
+++ douban-cover.php
@@ -1,10 +1,11 @@
 <?php
 /*
 Plugin Name: 豆瓣电影封面
-Description: 快速获取豆瓣电影封面及影片信息展示在博客中。
+Description: 快速获取豆瓣电影封面及影片信息展示在页面中。
 Version: 1.0.2
 Author: lylares
 Author URI: https://www.lylares.com
+Plugin URI: https://www.lylares.com/a-wordpress-plugin-of-douban-movie.html
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0-standalone.html
 
@@ -23,7 +24,16 @@ with Douban cover . If not, see
 https://www.gnu.org/licenses/gpl-3.0-standalone.html.
 */
 //global $wpdb;
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
 
-define("douban_cover",plugin_dir_path(__FILE__));
+define("DOUBANCOVER","ABCFC");
 
-require("init.php");
+define('SYSTEM_DOUBAN_COVER_ROOT', dirname(__FILE__));
+
+define('SYSTEM_DOUBAN_COVER', plugins_url('',__FILE__));
+
+define("DOUBAN_API","https://api-cn.berryapi.net/?service=App.Douban.Subject&AppKey=Test201901&id=");
+
+require SYSTEM_DOUBAN_COVER_ROOT.'/inc/core.php';
